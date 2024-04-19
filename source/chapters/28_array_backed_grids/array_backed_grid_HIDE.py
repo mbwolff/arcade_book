08bc701069b488571886fdb6064baf342b1f2df2
mbwolff <wolff.mark.b@gmail.com>
2024-04-19 07:56:37 -0400

    Create array_backed_grid_HIDE.py

---
+++ source/chapters/28_array_backed_grids/array_backed_grid_HIDE.py
@@ -0,0 +1,8 @@
+"""
+Array Backed Grid
+
+Show how to use a two-dimensional list/array to back the display of a
+grid on-screen.
+
+This file is purposely hidden so that you build the program yourself.
+"""
